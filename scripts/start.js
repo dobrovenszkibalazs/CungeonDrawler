959622e8bf780f206b5e6acdb1a2099d984ab42f
dobrovenszki.balazs <dobrovenszki.balazs@ckik.hu>
2025-05-15 07:54:28 +0200

    Elkezdtem... alapok vázolása

---
+++ scripts/start.js
@@ -0,0 +1,8 @@
+let menu = document.getElementById("main-menu");
+let main = document.getElementById("main-game")
+
+function start() {
+    console.log("anyu")
+    menu.style.display = "none";
+    main.style.display = "block";
+}
